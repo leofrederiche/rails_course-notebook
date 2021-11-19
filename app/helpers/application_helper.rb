4948d40123eff0da0e6b589d8c7ba6fc4c086f41
Leonardo Frederiche <leofrederiche@gmail.com>
2021-11-19 19:38:26 -0300

    Option select for State && Correction to Update Addresses from @contact.update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    BRAZILIAN_STATES = [
+        ["São Paulo", "SP"],
+        ["Rio de Janeiro", "RJ"],
+        ["Bahia", "BA"],
+        ["Belo Horizonte", "BH"],
+        ["Paraná", "PR"]
+    ]
+
+    def options_for_states(selected)
+        options_for_select(BRAZILIAN_STATES, selected)
+    end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :kind, optional: true # Optional is required in Rails 6 to optional relation
-  has_one :address
+  has_one :address, :dependent => :destroy # Require dependent: :destroy to UPDATE address from @contact.update
 
   accepts_nested_attributes_for :address
 end

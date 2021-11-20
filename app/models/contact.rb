3846d30b2c9be6bda45ff320c237bbe7ca49aa6e
Leonardo Frederiche <leofrederiche@gmail.com>
2021-11-19 21:43:47 -0300

    Include Phone association in Contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,7 @@
 class Contact < ApplicationRecord
   belongs_to :kind, optional: true # Optional is required in Rails 6 to optional relation  
-  has_one :address
+  has_one :address, dependent: :destroy
+  has_one :phone, dependent: :destroy
 
-  accepts_nested_attributes_for :address, allow_destroy: true, update_only: true
+  accepts_nested_attributes_for :address, update_only: true
 end

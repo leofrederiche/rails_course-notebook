b85fc10343abb30df5f1fc66e3f0c0d777ef40b2
Leonardo Frederiche <leofrederiche@gmail.com>
2021-11-19 21:11:41 -0300

    Removing comma

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,5 @@ class Contact < ApplicationRecord
   belongs_to :kind, optional: true # Optional is required in Rails 6 to optional relation  
   has_one :address
 
-  accepts_nested_attributes_for :address, allow_destroy: true, update_only: true, 
+  accepts_nested_attributes_for :address, allow_destroy: true, update_only: true
 end
